b920c194ba7e783c29347c3a960ef33437d4eef1
AaronXue <her8153101@yahoo.com.tw>
2020-06-04 13:07:22 +0800

    Add player idle, run, attack
    Setup sprites interface, player basic control.
    1. Press space to attack.
    2. w/a/s/d to move player.

---
+++ Assets/Scripts/SetupPlayer/Interfaces/IBasicControl.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBasicControl
+{
+    void DoAttack(Vector2 str);
+    void DoMove(Vector2 kinetic);
+    void DoSkill(int mode);
+} 
---
+++ Assets/Scripts/SetupPlayer/ScriptableObjects/FloatVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Game.Attritubes
+{
+    [SerializeField]
+    [CreateAssetMenu(menuName = "Attritubes/SPlayer")]
+    public class PlayerDatas : ScriptableObject
+    {
+    }
+}
