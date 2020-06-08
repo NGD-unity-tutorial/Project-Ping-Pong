04a207a53be8258c7c1cd16cbc5dfae86a5086b0
edisonsu0503 <edisonsu0503@gmail.com>
2020-06-08 17:00:40 +0800

    Add wormHole

--- Assets/Scripts/Classes/Ball/BallController.cs
+++ Assets/Scripts/Classes/Ball/BallController.cs
@@ -6,37 +6,39 @@ public class BallController : MonoBehaviour
 {
     // Start is called before the first frame update
     private float speed=10;
+    private Vector2 direction;
+    private float disappear;
+    private bool disCountUp = true;
+    private Color32 color;
+    private bool isThrough;           //判斷是否穿過蟲洞
+    private float timer;
+
+    private Vector3 wormHole_1_Pos;
+    private Vector3 wormHole_2_Pos;
+
+    public GameObject wormHole_1;
+    public GameObject wormHole_2;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
     public float angle=0;
 
-    private Vector2 direction;
+
     void Start()
     {
+        disappear = 255;
         speed = 10;
         angle = 0;
         CountDir();
+        Disappear();
+        SetColor();
+        isThrough = false;
+        timer = 0f;
     }
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.name == "Up")
-        {
-            angle = (angle)*-1;
-        }
-        if (collision.gameObject.name == "Down")
-        {
-            angle = (angle) * -1;
-        }
-        if (collision.gameObject.name == "Left")
-        {
-            angle = (angle-180) * -1;
-        }
-        if (collision.gameObject.name == "Right")
-        {
-            angle = (angle-180) * -1;
-        }
-    }
+
     // Update is called once per frame
     void Update()
     {
+        
         if (angle >= 360)
         {
             angle -= 360;
@@ -46,8 +48,18 @@ public class BallController : MonoBehaviour
             angle += 360;
         }
         CountDir();
-        transform.Translate(direction * speed * Time.deltaTime);
-        
+        transform.eulerAngles = new Vector3(0, 0, angle);           //決定火球飛行方向
+        transform.Translate(direction * speed * Time.deltaTime);    //火球前進
+        if (Input.GetKey(KeyCode.Q))
+        {
+            //animator.
+            Disappear();
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            disappear=255;
+        }
+
         if (Input.GetKey(KeyCode.R))
         {
             angle +=3 ;
@@ -112,11 +124,87 @@ public class BallController : MonoBehaviour
                 angle++;
             }
         }
-
+        if (isThrough == true)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 0.5)
+            {
+                timer = 0;
+                isThrough = false;
+            }
+        }
     }
 
     void CountDir()
     {
-        direction = new Vector2(Mathf.Cos(angle * (Mathf.PI / 180)), Mathf.Sin(angle * (Mathf.PI / 180)));
+        direction = new Vector2(1,0);
+        //direction = new Vector2(Mathf.Cos(angle * (Mathf.PI / 180)), Mathf.Sin(angle * (Mathf.PI / 180)));
+    }
+
+    void Disappear()
+    {
+        SetColor();
+        color.a = (byte)(disappear);
+        if (disappear >= 255)
+        {
+            disCountUp = false;
+        }
+        if (disappear <= 1)
+        {
+            disCountUp = true;
+        }
+        if (disCountUp)
+        {
+            disappear +=2;
+        }
+        else
+        {
+            disappear -=2;
+        }
+        spriteRenderer.color = color;
+    }
+    void SetColor()
+    {
+        color.r = 255;
+        color.b = 255;
+        color.g = 255;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Up")
+        {
+            angle = (angle) * -1;
+        }
+        if (collision.gameObject.name == "Down")
+        {
+            angle = (angle) * -1;
+        }
+        if (collision.gameObject.name == "Left")
+        {
+            angle = (angle - 180) * -1;
+        }
+        if (collision.gameObject.name == "Right")
+        {
+            angle = (angle - 180) * -1;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        wormHole_1_Pos = Transform.position_1;
+        wormHole_2_Pos = Transform.position_2;
+        if (isThrough == false)
+        {
+            if (collision.gameObject.CompareTag("WormHole_1"))
+            {
+                transform.position = wormHole_2_Pos;
+
+            }
+            if (collision.gameObject.CompareTag("WormHole_2"))
+            {
+                transform.position = wormHole_1_Pos;
+
+            }
+            isThrough = true;
+        }
     }
 }
